9a696c784e45e90bfdfdfce0c4a8c42e4a8051d8
David Fonseca <dafonsec@redhat.com>
2025-07-10 19:28:35 -0500

    ajuste

--- kafka.py
+++ kafka.py
@@ -1,20 +1,26 @@
 from pyspark.sql import SparkSession
 
-# Spark Session
+
+# Obtener variables del entorno con valores por defecto
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "test:443")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "test")
+KAFKA_TRUSTSTORE_LOCATION = os.getenv("KAFKA_TRUSTSTORE_LOCATION", "/Users/davidfonseca/Desktop/telefonica/kafka/client.truststore.jks")
+KAFKA_TRUSTSTORE_PASSWORD = os.getenv("KAFKA_TRUSTSTORE_PASSWORD", "123456789")
+
+# Crear Spark Session
 spark = SparkSession.builder \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,org.apache.kafka:kafka-clients:2.8.0") \
     .appName("KafkaSSLConsumer") \
     .getOrCreate()
 
-
-# Read from Kafka
+# Leer desde Kafka
 df = spark.readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka-cluster-kafka-external-bootstrap-prod-broker.apps.epico-seed-col-1.tmve.local:443") \
-    .option("subscribe", "test") \
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
+    .option("subscribe", KAFKA_TOPIC) \
     .option("kafka.security.protocol", "SSL") \
-    .option("kafka.ssl.truststore.location", "/Users/davidfonseca/Desktop/telefonica/kafka/client.truststore.jks") \
-    .option("kafka.ssl.truststore.password", "123456789") \
+    .option("kafka.ssl.truststore.location", KAFKA_TRUSTSTORE_LOCATION) \
+    .option("kafka.ssl.truststore.password", KAFKA_TRUSTSTORE_PASSWORD) \
     .load()
 
 # Convert binary values to string
